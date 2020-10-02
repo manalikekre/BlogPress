b488549dcafbb345a6987b4600059837676a19cc
manalikekre <manali.kekre@gmail.com>
2020-10-02 20:07:17 +0530

    created generic post component

--- front-end/app/components/Home.js
+++ front-end/app/components/Home.js
@@ -1,24 +1,68 @@
-import React, { useEffect, useContext } from "react"
-import Page from "./Page"
-import StateContext from "../StateContext"
+import React, { useEffect, useContext } from 'react';
+import Page from './Page';
+import StateContext from '../StateContext';
+import { useImmer } from 'use-immer';
+import LoadingIcon from './LoadingIcon';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
+import Post from './Post';
 
 function Home() {
-  const appState = useContext(StateContext)
-  return (
-    <Page title="Your feed">
-      <div className="container container--narrow py-md-5">
-        <h2 className="text-center">
-          Hello <strong>{appState.user.username}</strong>, your feed is empty.
-        </h2>
-        <p className="lead text-muted text-center">
-          Your feed displays the latest posts from the people you follow. If you
-          don&rsquo;t have any friends to follow that&rsquo;s okay; you can use
-          the &ldquo;Search&rdquo; feature in the top menu bar to find content
-          written by people with similar interests and then follow them.
-        </p>
-      </div>
-    </Page>
-  )
+	const appState = useContext(StateContext);
+	const [state, setState] = useImmer({
+		isLoading: true,
+		feed: [],
+	});
+
+	useEffect(() => {
+		try {
+			async function fetchProfile() {
+				const response = await Axios.post('/getHomeFeed', {
+					token: appState.user.token,
+				});
+				setState((draft) => {
+					draft.isLoading = false;
+					draft.feed = response.data;
+				});
+			}
+			fetchProfile();
+		} catch (e) {
+			console.log('problem', e);
+		}
+	}, []);
+
+	if (StateContext.isLoading) {
+		return <LoadingIcon />;
+	}
+
+	return (
+		<Page title="Your feed">
+			{state.feed.length > 0 && (
+				<>
+					<h2 className="text-center mb-4">The Latest from those you follow</h2>
+					<div className="list-group">
+						{state.feed.map((post) => {
+							return <Post post={post} key={post._id} />;
+						})}
+					</div>
+				</>
+			)}
+			{state.feed.length == 0 && (
+				<div className="container container--narrow py-md-5">
+					<h2 className="text-center">
+						Hello <strong>{appState.user.username}</strong>, your feed is empty.
+					</h2>
+					<p className="lead text-muted text-center">
+						Your feed displays the latest posts from the people you follow. If
+						you don&rsquo;t have any friends to follow that&rsquo;s okay; you
+						can use the &ldquo;Search&rdquo; feature in the top menu bar to find
+						content written by people with similar interests and then follow
+						them.
+					</p>
+				</div>
+			)}
+		</Page>
+	);
 }
 
-export default Home
+export default Home;
--- front-end/app/components/ProfilePosts.js
+++ front-end/app/components/ProfilePosts.js
@@ -1,52 +1,37 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from 'react';
 
-import Axios from "axios"
-import { useParams, Link } from "react-router-dom"
-import LoadingIcon from "./LoadingIcon"
+import Axios from 'axios';
+import { useParams, Link } from 'react-router-dom';
+import LoadingIcon from './LoadingIcon';
+import Post from './Post';
 
 function ProfilePosts() {
-  const { username } = useParams()
-  const [posts, setPosts] = useState([])
-  const [isLoading, setLoading] = useState(true)
+	const { username } = useParams();
+	const [posts, setPosts] = useState([]);
+	const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function getPosts() {
-      try {
-        const response = await Axios.get(`/profile/${username}/posts`)
-        setPosts(response.data)
-        setLoading(false)
-      } catch (e) {
-        console.log("error - ", e)
-      }
-    }
-    getPosts()
-  }, [username])
+	useEffect(() => {
+		async function getPosts() {
+			try {
+				const response = await Axios.get(`/profile/${username}/posts`);
+				setPosts(response.data);
+				setLoading(false);
+			} catch (e) {
+				console.log('error - ', e);
+			}
+		}
+		getPosts();
+	}, [username]);
 
-  if (isLoading) return <LoadingIcon />
+	if (isLoading) return <LoadingIcon />;
 
-  return (
-    <div className="list-group">
-      {posts.map((post) => {
-        post
-        const date = new Date(post.createdDate)
-        const formattedDate = `${date.getDate()}/${
-          date.getMonth() + 1
-        }/${date.getFullYear()}`
-
-        return (
-          <Link
-            key={post._id}
-            to={`/post/${post._id}`}
-            className="list-group-item list-group-item-action"
-          >
-            <img className="avatar-tiny" src={post.author.avatar} />{" "}
-            <strong>{post.title}</strong>
-            <span className="text-muted small"> on {formattedDate} </span>
-          </Link>
-        )
-      })}
-    </div>
-  )
+	return (
+		<div className="list-group">
+			{posts.map((post) => {
+				return <Post isAuthor={true} post={post} key={post._id} />;
+			})}
+		</div>
+	);
 }
 
-export default ProfilePosts
+export default ProfilePosts;
